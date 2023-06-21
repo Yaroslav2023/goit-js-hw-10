b109c1e924eb65158985bc9d716fb9b78cfb8c13
Yaroslav2023 <yishevchenko@ukr.net>
2023-06-21 21:44:35 +0300

    tasks-complate

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 
 errorHidden();
-selectHidden()
+selectHidden();
 creatSelectOptions ();
 
 function creatSelectOptions () {
     fetchBreeds().then((breeds) => {renderOptions(breeds);
-    loaderHidden()
+    loaderHidden();
+    selectNotHidden();
 });
 }
 
@@ -29,15 +30,16 @@ function renderOptions(breeds) {
 selectEl.addEventListener('change', onBreedSelect);
 
 function onBreedSelect () {
-  clearCatInfo()
+  clearCatInfo();
+  loaaderNotHidden();
   let breedId = selectEl.value;
-  fetchCatByBreed(breedId).then((objCat) => renderArticle(objCat));
-  
+  fetchCatByBreed(breedId).then((objCat) => {renderArticle(objCat);
+    loaderHidden();});
 }
 
 function renderArticle(objCat) {
   const markupBreedDescr = objCat.map(({url, breeds }) => {
-    return `<img src="${url}" alt="" width="300px">
+    return `<img src="${url}" alt="" width="600px">
     <div><h2>${breeds[0].name}</h2>
     <p>${breeds[0].description}</p>
     <p>Temperament: ${breeds[0].temperament}</p></div>
@@ -55,11 +57,19 @@ function loaderHidden() {
   loaderEl.classList.add('visually-hidden');
 }
 
+function loaaderNotHidden() {
+  loaderEl.classList.remove('visually-hidden');
+}
+
 function errorHidden() {
   errorEl.classList.add('visually-hidden');
 }
 
 function selectHidden() {
   selectEl.classList.add('visually-hidden');
+}
+
+function selectNotHidden() {
+  selectEl.classList.remove('visually-hidden');
 }
 
